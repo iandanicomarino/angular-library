f3ced95a7ae6103e1f82d01910cce57eb045eb5d
nekomarino <nekomarino@gmail.com>
2017-02-13 14:02:48 +0800

    initial setup for angular ui router

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-angular.module('myApp', ['ngMaterial']);
+var myApp = angular.module('myApp', 
+    [
+        'ngMaterial',
+        'ui.router'
+    ]
+)
+
+myApp.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});
